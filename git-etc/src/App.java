96a40a6ffbe1fa3f3d69c517510b8cc13e9c2eaf
superthisme <apprentice91@gmail.com>
2020-01-10 15:00:46 +0900

    freedom

--- git-etc/src/App.java
+++ git-etc/src/App.java
@@ -2,10 +2,12 @@
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("아무거나");
 
 		System.out.println("----------------------");
 		System.out.println("왔노라 보았노라 이겼노라!");
+		
+		System.out.println("----------------------");
+		System.out.println("영환 수정");
 	}
 
 }
