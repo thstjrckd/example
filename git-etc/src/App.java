bcfb1054d8af317ba079848f61f4860170572e49
thstjrckd <58419760+thstjrckd@users.noreply.github.com>
2020-01-10 15:00:51 +0900

    update!!

--- git-etc/src/App.java
+++ git-etc/src/App.java
@@ -6,6 +6,9 @@ public class App {
 
 		System.out.println("----------------------");
 		System.out.println("왔노라 보았노라 이겼노라!");
+		
+		System.out.println("----------------------");
+		System.out.println("배가 붚러서 졸리다...");
 	}
 
 }
